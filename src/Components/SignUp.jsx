7e803dcc900e5ed47af37ddfac1ff92c4746d8a2
SathiyaNallathambi <sathiyaece477@gmail.com>
2025-01-08 22:23:56 +0530

    working validations

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -55,7 +55,7 @@ export default function SignUp(){
     if (!passwordRegex.test(addingData?.password)) {
         newErrors.password = "Password must contain at least 1 uppercase letter, 1 digit, and 1 special character.";
     }
-    if (!addingData?.mobileNo || addingData?.mobileNo.length !==10) {
+    if (!addingData?.mobileNo || addingData?.mobileNo.length!==10) {
         newErrors.mobileNo = "Mobile number must be 10 digits.";
     }
 
